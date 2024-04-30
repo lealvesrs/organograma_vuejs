3fbfb92466bf23b67829a7fc602ae0aa27fa29df
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-30 15:22:08 -0300

    feat: criando componente de card da tela inicial

---
+++ src/components/CardHome.vue
@@ -0,0 +1,27 @@
+<template>
+    <router-link :to="path"><v-card class="py-4" color="indigo-darken-2" :prepend-icon="icon"
+            rounded="lg" variant="flat">
+
+            <template #title>
+                <h2 class="text-md-h5 text-h6 font-weight-bold text-wrap">{{title}}</h2>
+            </template>
+
+            <template #subtitle>
+                <div class="text-subtitle-1 text-wrap">
+                    {{subtitle}}
+                </div>
+            </template>
+
+        </v-card></router-link>
+</template>
+
+<script>
+export default {
+    props:{
+        path:String,
+        icon:String,
+        title:String,
+        subtitle:String
+    }
+}
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,6 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive
-      class="align-center fill-height mx-auto"
-      max-width="70%"
-    >
+    <v-responsive class="align-center fill-height mx-auto" max-width="70%">
 
       <div class="text-center pb-12">
         <div class="text-body-2 font-weight-light mb-n1">Vue.js & Vuetify</div>
@@ -15,83 +12,43 @@
       <v-row>
 
         <v-col cols="12">
-          <router-link to="/times"><v-card
-            class="py-4"
-            color="indigo-darken-2"
-            prepend-icon="mdi mdi-family-tree"
-            rounded="lg"
-            variant="flat"
-          >
-  
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">Ver organograma</h2>
-            </template>
+          <CardHome path="/times" title="Ver organograma" subtitle="Ver organograma" icon="mdi mdi-family-tree">
+          </CardHome>
 
-            <template #subtitle>
-              <div class="text-subtitle-1">
-                Veja os times e seus colaboradores
-              </div>
-            </template>
-            
-          </v-card></router-link>
         </v-col>
-        <v-col cols="6">
-          <router-link to="/cadColaborador"><v-card
-            class="py-4"
-            color="indigo-darken-2"
-            prepend-icon="mdi mdi-account-multiple-plus"
-            rounded="lg"
-            variant="flat"
-          >
-  
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">Cadastrar novo colaborador</h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-subtitle-1">
-                Adicione um novo membro ao time
-              </div>
-            </template>
-            
-          </v-card></router-link>
+        <v-col :cols="cols()">
+          <CardHome path="/cadColaborador" title="Cadastrar novo colaborador" subtitle="Adicione um novo membro ao time"
+            icon="mdi mdi-account-multiple-plus">
+          </CardHome>
         </v-col>
 
-        <v-col cols="6">
-          <router-link to="/cadTimes"><v-card
-            class="py-4"
-            color="indigo-darken-2"
-            prepend-icon="mdi mdi-view-grid-plus"
-            rounded="lg"
-            variant="flat"
-          >
-  
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">Cadastrar novo time</h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-subtitle-1">
-                Adicione um novo time
-              </div>
-            </template>
-            
-          </v-card></router-link>
+        <v-col :cols="cols()">
+          <CardHome path="/cadTimes" title="Cadastrar novo time" subtitle="Adicione um novo time"
+            icon="mdi mdi-view-grid-plus">
+          </CardHome>
         </v-col>
 
-      </v-row> 
+      </v-row>
     </v-responsive>
   </v-container>
 </template>
 
-<script setup>
-
-  //
+<script>
+export default {
+  methods: {
+    cols() {
+      if (this.$vuetify.display.mobile) {
+        return '12'
+      } else {
+        return '6'
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
 a {
   text-decoration: none;
 }
-
 </style>
